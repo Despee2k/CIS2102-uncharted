c26dc568ab8f9438da844770b826a056ab48f130
alphas12 <146078143+alphas12@users.noreply.github.com>
2024-12-12 13:32:11 +0800

    feat: add theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+      "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              dominant: '#FAF7F0',  // Light Greyish Tone
+              primary: '#4A4947',   // Medium Dark
+              secondary: '#D8D2C2',
+              accent: '#B17457'      // Background Light Color
+           },
+
+          fontFamily: {
+              heading: ['Poppins', 'sans-serif'], // For headers
+              body: ['Open Sans', 'sans-serif'],      // For body text
+          },
+      },
   },
   plugins: [],
 }
-
