8a5b89cc069b5df3ced7a44ab8937b282a887e91
alphas12 <146078143+alphas12@users.noreply.github.com>
2024-12-12 13:31:19 +0800

    create footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-[#FAF7F0] text-black py-4 text-center font-regular">
+          <p>&copy; 2024 Uncharted Creatives. All rights reserved.</p>
+      </footer>
+    )
+  }
+  
+  export default Footer
