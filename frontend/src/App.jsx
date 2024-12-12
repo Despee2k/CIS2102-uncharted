e138edb83facb18edb961f128517384b61d492c6
alphas12 <146078143+alphas12@users.noreply.github.com>
2024-12-12 12:12:59 +0800

    feat: add ContactUs component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import HomePage from './pages/NewHomePage';
+// import FlipBookPage from './pages/FlipBookPage';
+// import LoginPage from './pages/LoginPage';
+// import SignupPage from './pages/SignupPage';
+import ContactPage from './pages/ContactPage';
+// import RecipePage from './pages/RecipePage';
+// import AddRecipePage from './pages/AddRecipePage';
 
 const App = () => {
-    return (
-        <div>
-            <Navbar />
-            {/* Other components go here */}
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} />
+        <Route path="/flipbookpage" element={<FlipBookPage />} />
+        <Route path="/recipepage" element={<RecipePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} /> */}
+        <Route path="/contact" element={<ContactPage />} />
+        {/* <Route path="/addrecipepage" element={<AddRecipePage />} /> */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/ContactPage.jsx
@@ -0,0 +1,33 @@
+
+
+import ContactUsSection from "../features/ContactUsSection";
+import Navbar from "../components/Navbar";
+import JoinUsCTA from "../components/ContactUs/JoinUsCTA";
+import Footer from "../components/Footer";
+import Steak from "../assets/Steak.png";
+
+
+const ContactPage = () => {
+    return (
+        <main>
+            <Navbar />
+            {/* Hero image or banner */}
+            <div
+                className="relative h-64 md:h-80 bg-cover bg-center"
+                style={{ backgroundImage: `url(${Steak})` }}
+            >
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <h1 className="text-white text-4xl md:text-5xl font-bold">Contact Us</h1>
+                </div>
+            </div>
+
+            {/* Contact Us Section */}
+            <ContactUsSection />
+            <JoinUsCTA />
+            <Footer />
+        </main>
+    );
+};
+
+export default ContactPage;
